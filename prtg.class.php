6cdf65bc34854991ac692bd34c468d81f7ce00c7
Amir Keshavarz <amirkekh@gmail.com>
2017-08-03 08:51:04 +0430

    Tab size problem

--- prtg.class.php
+++ prtg.class.php
@@ -39,7 +39,7 @@ class prtg {
     	curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
     	curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
     	curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, 0);
-		curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, 0);
+    	curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, 0);
     	$response = curl_exec($ch);
     	curl_close($ch);
     	return $response;
