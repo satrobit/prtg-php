4109965982322e173d437177b734e0e5595965d8
Amir Keshavarz <amirkekh@gmail.com>
2017-08-03 09:36:29 +0430

    formatting

--- prtg.class.php
+++ prtg.class.php
@@ -34,15 +34,15 @@ class prtg {
 	*/
 	private function sendRequest($url)
 	{
-    	$ch = curl_init();
-    	curl_setopt($ch, CURLOPT_URL, $url);
-    	curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-    	curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
-    	curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, 0);
-    	curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, 0);
-    	$response = curl_exec($ch);
-    	curl_close($ch);
-    	return $response;
+		$ch = curl_init();
+		curl_setopt($ch, CURLOPT_URL, $url);
+		curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+		curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
+		curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, 0);
+		curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, 0);
+		$response = curl_exec($ch);
+		curl_close($ch);
+		return $response;
 	}
 
 	/**
